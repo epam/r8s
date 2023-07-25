bdb08f60c61b65417d52e1bc0f9f6557c0fa94f4
Bohdan Onsha <bohdan_onsha@epam.com>
2023-07-25 16:54:52 +0300

    WIP: update parent processor, implement license activation on parent creation

--- src/lambdas/r8s_api_handler/processors/parent_processor.py
+++ src/lambdas/r8s_api_handler/processors/parent_processor.py
@@ -10,18 +10,22 @@ from modular_sdk.services.tenant_service import TenantService
 
 from commons import RESPONSE_BAD_REQUEST_CODE, raise_error_response, \
     build_response, RESPONSE_RESOURCE_NOT_FOUND_CODE, RESPONSE_OK_CODE, \
-    validate_params
+    validate_params, RESPONSE_FORBIDDEN_CODE
 from commons.abstract_lambda import PARAM_HTTP_METHOD
 from commons.constants import GET_METHOD, POST_METHOD, PATCH_METHOD, \
     DELETE_METHOD, PARENT_ID_ATTR, APPLICATION_ID_ATTR, DESCRIPTION_ATTR, \
     ALGORITHM_ATTR, CLOUD_ATTR, SCOPE_ATTR, \
     CLOUD_ALL, ALLOWED_PARENT_SCOPES, \
-    PARENT_SCOPE_SPECIFIC_TENANT, TENANT_ATTR
+    PARENT_SCOPE_SPECIFIC_TENANT, TENANT_ATTR, TENANT_LICENSE_KEY_ATTR, \
+    LICENSE_KEY_ATTR
 from commons.log_helper import get_logger
 from lambdas.r8s_api_handler.processors.abstract_processor import \
     AbstractCommandProcessor
 from models.algorithm import Algorithm
+from models.license import License
 from services.algorithm_service import AlgorithmService
+from services.license_manager_service import LicenseManagerService
+from services.license_service import LicenseService
 from services.rightsizer_application_service import \
     RightSizerApplicationService
 from services.rightsizer_parent_service import RightSizerParentService
@@ -36,12 +40,16 @@ class ParentProcessor(AbstractCommandProcessor):
                  customer_service: CustomerService,
                  application_service: RightSizerApplicationService,
                  parent_service: RightSizerParentService,
-                 tenant_service: TenantService):
+                 tenant_service: TenantService,
+                 license_service: LicenseService,
+                 license_manager_service: LicenseManagerService):
         self.algorithm_service = algorithm_service
         self.customer_service = customer_service
         self.application_service = application_service
         self.parent_service = parent_service
         self.tenant_service = tenant_service
+        self.license_service = license_service
+        self.license_manager_service = license_manager_service
 
         self.method_to_handler = {
             GET_METHOD: self.get,
@@ -107,10 +115,12 @@ class ParentProcessor(AbstractCommandProcessor):
 
     def post(self, event):
         _LOG.debug(f'Create parent event: {event}')
-        validate_params(event, (APPLICATION_ID_ATTR,
-                                DESCRIPTION_ATTR, ALGORITHM_ATTR, CLOUD_ATTR,
-                                SCOPE_ATTR))
-
+        validate_params(event, (APPLICATION_ID_ATTR, DESCRIPTION_ATTR,
+                                CLOUD_ATTR, SCOPE_ATTR))
+        if not event.get(ALGORITHM_ATTR) and \
+                not event.get(TENANT_LICENSE_KEY_ATTR):
+            _LOG.error(f'One of the following parameters must be specified: '
+                       f'{ALGORITHM_ATTR}, {TENANT_LICENSE_KEY_ATTR}')
         _LOG.debug(f'Resolving applications')
         applications = self.application_service.resolve_application(
             event=event)
@@ -151,7 +161,24 @@ class ParentProcessor(AbstractCommandProcessor):
                         f'Available clouds: {", ".join(CLOUDS)}'
             )
 
-        algorithm = event.get(ALGORITHM_ATTR)
+        license_key = None
+        tenant_license_key = event.get(TENANT_LICENSE_KEY_ATTR)
+        if tenant_license_key:
+            _LOG.debug(f'Activating license \'{tenant_license_key}\' '
+                       f'for customer')
+            license_obj = self.activate_license(
+                tenant_license_key=tenant_license_key,
+                customer=customer
+            )
+            self._execute_license_sync(
+                license_obj=license_obj,
+                customer=customer
+            )
+            license_key = license_obj.license_key
+            algorithm = license_obj.algorithm_id
+        else:
+            algorithm = event.get(ALGORITHM_ATTR)
+
         _LOG.debug(f'Validating algorithm \'{algorithm}\'')
         algorithm_obj: Algorithm = self.algorithm_service.get_by_name(
             name=algorithm)
@@ -236,7 +263,8 @@ class ParentProcessor(AbstractCommandProcessor):
             description=description,
             cloud=cloud,
             algorithm=algorithm_obj,
-            scope=scope
+            scope=scope,
+            license_key=license_key
         )
 
         parent_dto = self.parent_service.get_dto(parent=parent)
@@ -445,3 +473,53 @@ class ParentProcessor(AbstractCommandProcessor):
                 linked_tenant_names.append(tenant.name)
 
         return linked_tenant_names
+
+    def activate_license(self, tenant_license_key: str, customer: str):
+        _response = self.license_manager_service.activate_customer(
+            customer, tenant_license_key
+        )
+        if not _response:
+            _message = f'License manager does not allow to activate ' \
+                       f'tenant license \'{tenant_license_key}\'' \
+                       f' for customer \'{customer}\''
+            _LOG.warning(_message)
+            return build_response(code=RESPONSE_FORBIDDEN_CODE,
+                                  content=_message)
+        license_key = _response.get(LICENSE_KEY_ATTR)
+        license_obj = self.license_service.get_license(license_key)
+        if not license_obj:
+            _LOG.info(f'License object with id \'{license_key}\' does '
+                      f'not exist yet. Creating.')
+            license_obj = self.license_service.create({
+                LICENSE_KEY_ATTR: license_key})
+        if not license_obj.customers or not license_obj.customers.get(
+                customer):
+            license_obj.customers = {customer: {}}
+
+        license_obj.customers.get(customer)[
+            TENANT_LICENSE_KEY_ATTR] = tenant_license_key
+        _LOG.info('Going to save license object')
+        license_obj.save()
+
+        return license_obj
+
+    def _execute_license_sync(self, license_obj: License, customer: str):
+        _LOG.info(f'Syncing license \'{license_obj.license_key}\'')
+        response = self.license_manager_service.synchronize_license(
+            license_key=license_obj.license_key)
+        if not response.status_code == 200:
+            return
+
+        license_data = response.json()['items'][0]
+
+        _LOG.debug(f'Updating license {license_obj.license_key}')
+        license_obj = self.license_service.update_license(
+            license_obj=license_obj,
+            license_data=license_data
+        )
+        _LOG.debug(f'Updating licensed algorithm')
+        self.algorithm_service.sync_licensed_algorithm(
+            license_data=license_data,
+            customer=customer
+        )
+        return license_obj
