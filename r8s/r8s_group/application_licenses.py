8bb33abcd5fc419b04b877e04d7f071fcfecf0f7
bohdan-onsha <onsha.bogdan@gmail.com>
2023-11-29 10:28:24 +0200

    WIP: add application licenses command group

---
+++ r8s/r8s_group/application_licenses.py
@@ -0,0 +1,61 @@
+import click
+
+from r8s_group import cli_response, ViewCommand
+from r8s_service.constants import ALLOWED_PROTOCOLS, \
+    PROTOCOL_HTTPS, AVAILABLE_CLOUDS
+
+
+@click.group(name='licenses')
+def licenses():
+    """Manages RIGHTSIZER_LICENSES Application Entity"""
+
+
+@licenses.command(cls=ViewCommand, name='describe')
+@click.option('--application_id', '-aid', type=str, required=False,
+              help='Id of the application to describe.')
+@cli_response()
+def describe(application_id=None):
+    """
+    Describes a RIGHTSIZER_LICENSES Application.
+    """
+    from r8s_service.initializer import init_configuration
+    return init_configuration().application_licenses_get(
+        application_id=application_id)
+
+
+@licenses.command(cls=ViewCommand, name='add')
+@click.option('--customer_id', '-cid', type=str, required=True,
+              help='Maestro Customer name.')
+@click.option('--description', '-d', type=str, required=True,
+              help='Application description.')
+@click.option('--cloud', '-c', required=True,
+              type=click.Choice(AVAILABLE_CLOUDS),
+              help='Price cloud.')
+@click.option('--tenant_license_key', '-tlk', required=True, type=str,
+              help='Tenant license key.')
+@cli_response()
+def add(customer_id, description, cloud, tenant_license_key):
+    """
+    Creates Maestro RIGHTSIZER_LICENSES Application
+    """
+    from r8s_service.initializer import init_configuration
+
+    return init_configuration().application_licenses_post(
+        customer=customer_id,
+        description=description,
+        cloud=cloud,
+        tenant_license_key=tenant_license_key
+    )
+
+
+@licenses.command(cls=ViewCommand, name='delete')
+@click.option('--application_id', '-aid', type=str, required=True,
+              help='Id of the application to delete.')
+@cli_response()
+def delete(application_id):
+    """
+    Deletes RIGHTSIZER_LICENSES Application.
+    """
+    from r8s_service.initializer import init_configuration
+    return init_configuration().application_licenses_delete(
+        application_id=application_id)
