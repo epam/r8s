bbb89026380c20fc96e63c103e10be8821f74b22
bohdan-onsha <onsha.bogdan@gmail.com>
2024-01-11 12:26:53 +0200

    remove unused code in docker/rightsizer_parent_service.py

--- docker/services/rightsizer_parent_service.py
+++ docker/services/rightsizer_parent_service.py
@@ -1,7 +1,6 @@
 from collections import defaultdict
-from typing import List, Union, Dict
+from typing import List, Dict
 
-from modular_sdk.commons import generate_id
 from modular_sdk.commons.constants import RIGHTSIZER_PARENT_TYPE, \
     TENANT_PARENT_MAP_RIGHTSIZER_TYPE, RIGHTSIZER_LICENSES_PARENT_TYPE, \
     TENANT_PARENT_MAP_RIGHTSIZER_LICENSES_TYPE, ParentScope
@@ -13,7 +12,6 @@ from pynamodb.attributes import MapAttribute
 
 from commons.constants import MAESTRO_RIGHTSIZER_LICENSES_APPLICATION_TYPE
 from commons.log_helper import get_logger
-from models.algorithm import Algorithm
 from models.parent_attributes import ShapeRule, LicensesParentMeta
 from services.environment_service import EnvironmentService
 
@@ -36,25 +34,6 @@ class RightSizerParentService(ParentService):
             tenant_service=tenant_service,
             customer_service=customer_service
         )
-
-    def create_rightsizer_licenses_parent(self, application_id, customer_id,
-                                          description, cloud: str,
-                                          algorithm: Algorithm,
-                                          scope: str, license_key: str = None):
-        meta = LicensesParentMeta(
-            cloud=cloud,
-            algorithm=algorithm.name,
-            license_key=license_key,
-        )
-        parent = self.create(
-            application_id=application_id,
-            customer_id=customer_id,
-            description=description,
-            is_deleted=False,
-            parent_type=RIGHTSIZER_LICENSES_PARENT_TYPE,
-            meta=meta.as_dict()
-        )
-        return parent
 
     @staticmethod
     def list_application_parents(application_id, type_: str,
@@ -98,106 +77,12 @@ class RightSizerParentService(ParentService):
             parent_meta_obj = LicensesParentMeta()
         return parent_meta_obj
 
-    def set_parent_meta(self, parent: Parent,
-                        meta: LicensesParentMeta):
-        meta_dict = meta.as_dict()
-
-        excess_attributes = self._excess_attributes_cache.get(
-            parent.parent_id)
-        if excess_attributes:
-            meta_dict.update(excess_attributes)
-
-        parent.meta = meta_dict
-
     @staticmethod
     def list_shape_rules(parent_meta: LicensesParentMeta) -> \
             List[ShapeRule]:
         if not parent_meta.shape_rules:
             return []
         return [ShapeRule(**rule) for rule in parent_meta.shape_rules]
-
-    def get_shape_rule(self, parent_meta: LicensesParentMeta,
-                       rule_id: str) -> Union[ShapeRule, None]:
-        rules = self.list_shape_rules(parent_meta=parent_meta)
-        if not rules:
-            return
-        for rule in rules:
-            if rule.rule_id == rule_id:
-                return rule
-
-    @staticmethod
-    def create_shape_rule(action, cloud, condition, field, value) -> ShapeRule:
-        return ShapeRule(rule_id=generate_id(),
-                         action=action, cloud=cloud, condition=condition,
-                         field=field, value=value)
-
-    def add_shape_rule_to_meta(self, parent_meta: LicensesParentMeta,
-                               shape_rule: ShapeRule):
-        shape_rules = self.list_shape_rules(parent_meta=parent_meta)
-        if not shape_rules:
-            shape_rules = list()
-            parent_meta.shape_rules = shape_rules
-
-        parent_meta.shape_rules.append(shape_rule)
-
-    def update_shape_rule_in_parent(self,
-                                    parent_meta: LicensesParentMeta,
-                                    shape_rule: ShapeRule):
-        shape_rules = self.list_shape_rules(parent_meta=parent_meta)
-        if not shape_rules:
-            return
-        for index, existing_rule in enumerate(shape_rules):
-            if existing_rule.rule_id == shape_rule.rule_id:
-                parent_meta.shape_rules[index] = shape_rule.as_dict()
-                return
-
-    @staticmethod
-    def update_shape_rule(shape_rule: ShapeRule, action=None, field=None,
-                          condition=None, value=None) -> None:
-        if action:
-            shape_rule.action = action
-        if condition:
-            shape_rule.condition = condition
-        if field:
-            shape_rule.field = field
-        if value:
-            shape_rule.value = value
-
-    def remove_shape_rule_from_application(self,
-                                           parent_meta: LicensesParentMeta,
-                                           rule_id: str) -> None:
-        shape_rules = self.list_shape_rules(parent_meta=parent_meta)
-        if not shape_rules:
-            return
-        for index, existing_rule in enumerate(shape_rules):
-            if existing_rule.rule_id == rule_id:
-                del parent_meta.shape_rules[index]
-                return
-
-    @staticmethod
-    def get_shape_rule_dto(shape_rule: ShapeRule):
-        return shape_rule.as_dict()
-
-    def filter_directly_linked_tenants(self, tenant_names: List[str],
-                                       parent: Parent):
-        specific_parents = self.query_by_scope_index(
-            customer_id=parent.customer_id,
-            type_=parent.type,
-            scope=ParentScope.SPECIFIC,
-            is_deleted=False
-        )
-        disabled_parents = self.query_by_scope_index(
-            customer_id=parent.customer_id,
-            type_=parent.type,
-            scope=ParentScope.DISABLED,
-            is_deleted=False
-        )
-        parents = [*list(specific_parents), *list(disabled_parents)]
-        exclude_tenant_names = {parent.tenant_name for parent
-                                in parents if parent.tenant_name}
-        _LOG.debug(f'Tenants will be excluded from scan: '
-                   f'{exclude_tenant_names}')
-        return list(set(tenant_names) - exclude_tenant_names)
 
     def resolve_tenant_parent_meta_map(
             self, parents: List[Parent]) -> Dict[str, LicensesParentMeta]:
