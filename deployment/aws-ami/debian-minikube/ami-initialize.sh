97d1fe03a53d0c8bda2ba677a8935ca281c60ae7
bohdan-onsha <onsha.bogdan@gmail.com>
2024-10-04 14:59:19 +0300

    add metric collector installation

--- deployment/aws-ami/debian-minikube/ami-initialize.sh
+++ deployment/aws-ami/debian-minikube/ami-initialize.sh
@@ -241,6 +241,7 @@ helm repo add syndicate s3://charts-repository/syndicate/
 helm repo update syndicate
 
 helm install "$HELM_RELEASE_NAME" syndicate/rightsizer --version $RIGHTSIZER_RELEASE
+helm install metric-collector syndicate/metric-collector
 helm install defectdojo syndicate/defectdojo
 EOF
 
