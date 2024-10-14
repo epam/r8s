efebd7aaa18698cabb715838cbbdaf63cbef8059
bohdan-onsha <onsha.bogdan@gmail.com>
2024-10-14 15:36:20 +0300

    TEMP: disable credentials hiding from logs

--- docker/commons/__init__.py
+++ docker/commons/__init__.py
@@ -19,21 +19,22 @@ def dateparse(time_in_secs):
 def secure_event(event: dict,
                  secured_keys=(PASSWORD_ATTR, ID_TOKEN_ATTR,
                                REFRESH_TOKEN_ATTR, AUTHORIZATION_PARAM)):
-    result_event = {}
-    if not isinstance(event, dict):
-        return event
-    for key, value in event.items():
-        if isinstance(value, dict):
-            result_event[key] = secure_event(
-                event=value,
-                secured_keys=secured_keys)
-        if isinstance(value, list):
-            result_event[key] = []
-            for item in value:
-                result_event[key].append(secure_event(item))
-        elif key in secured_keys:
-            result_event[key] = '*****'
-        else:
-            result_event[key] = secure_event(value)
-
-    return result_event
+    return event # todo remove
+    # result_event = {}
+    # if not isinstance(event, dict):
+    #     return event
+    # for key, value in event.items():
+    #     if isinstance(value, dict):
+    #         result_event[key] = secure_event(
+    #             event=value,
+    #             secured_keys=secured_keys)
+    #     if isinstance(value, list):
+    #         result_event[key] = []
+    #         for item in value:
+    #             result_event[key].append(secure_event(item))
+    #     elif key in secured_keys:
+    #         result_event[key] = '*****'
+    #     else:
+    #         result_event[key] = secure_event(value)
+    #
+    # return result_event
