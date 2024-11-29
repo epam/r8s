a2228db469b0ab1c6a9df1f3d1fdd92e58e03fd6
bohdan-onsha <onsha.bogdan@gmail.com>
2024-11-29 16:16:58 +0200

    update modular api admin policy to hide unused r8s commands

--- deployment/aws-ami/debian-minikube/r8s-init.sh
+++ deployment/aws-ami/debian-minikube/r8s-init.sh
@@ -254,6 +254,13 @@ patch_kubectl_secret() {
   secret=$(base64 <<< "$3")
   kubectl patch secret "$1" -p="{\"data\":{\"$2\":\"$secret\"}}"
 }
+update_modular_api_policy() {
+  modular_api_pod_name=$(kubectl get pods -n default -l app.kubernetes.io/name=modular-api -o jsonpath='{.items[0].metadata.name}')
+  echo $MODULAR_ADMIN_POLICY > /tmp/modular-admin-policy.json
+  kubectl cp /tmp/modular-admin-policy.json $modular_api_pod_name:/src/admin_policy.json
+  kubectl exec service/modular-api -- ./modular.py policy update --policy admin_policy --policy_path /src/admin_policy.json
+  rm /tmp/modular-admin-policy.json
+}
 
 initialize_system() {
   # creates:
@@ -271,6 +278,10 @@ initialize_system() {
 #  pip3 install --user --break-system-packages --upgrade "$R8S_RELEASES_PATH/$(get_latest_local_release)/${OBFUSCATOR_ARTIFACT_NAME}[xlsx]"
   echo "Installing modular-cli"
   MODULAR_CLI_ENTRY_POINT=$MODULAR_CLI_ENTRY_POINT pip3 install --user --break-system-packages --upgrade "$R8S_RELEASES_PATH/$(get_latest_local_release)/$MODULAR_CLI_ARTIFACT_NAME"
+
+  echo "Updating modular admin policy"
+  update_modular_api_policy
+  echo "Modular API policy has been updated."
 
   echo "Logging in to modular-cli"
   syndicate setup --username admin --password "$(get_kubectl_secret modular-api-secret system-password)" --api_path "http://$mip:32105" --json
@@ -771,6 +782,7 @@ MODULAR_CLI_ARTIFACT_NAME=modular_cli.tar.gz
 #OBFUSCATOR_ARTIFACT_NAME=r8s_obfuscator.tar.gz
 R8S_INIT_ARTIFACT_NAME=r8s-init.sh
 MODULAR_CLI_ENTRY_POINT=syndicate
+MODULAR_ADMIN_POLICY='[{"Description": "Admin policy", "Module": "*", "Effect": "Allow", "Resources": ["*"]}, {"Effect": "Deny", "Description": "Prohibited commands", "Module": "r8s", "Resources": ["algorithm:add", "algorithm:update_clustering_settings", "algorithm:update_general_settings", "algorithm:update_metric_format", "algorithm:update_recommendation_settings", "report:initiate_tenant_mail_report"]}]'
 FIRST_USER=$(getent passwd 1000 | cut -d : -f 1)
 
 case "$1" in
