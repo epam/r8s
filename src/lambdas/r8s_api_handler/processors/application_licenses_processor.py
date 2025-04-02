b1fd63b46b7e71c84618fd08d7b255f95d80197c
bohdan-onsha <onsha.bogdan@gmail.com>
2025-04-02 11:47:53 +0300

    forbid to delete RIGHTSIZER_LICENSES application if there're active linked parents

--- src/lambdas/r8s_api_handler/processors/application_licenses_processor.py
+++ src/lambdas/r8s_api_handler/processors/application_licenses_processor.py
@@ -209,12 +209,34 @@ class ApplicationLicensesProcessor(AbstractCommandProcessor):
                 code=RESPONSE_RESOURCE_NOT_FOUND_CODE,
                 content=f'Application {application_id} not found.'
             )
+        _LOG.debug(f'Searching for application {target_application.application_id} '
+                   f'parents')
+        parents = self.parent_service.list_application_parents(
+            application_id=target_application.application_id,
+            only_active=True
+        )
         force = event.get(FORCE_ATTR)
         try:
             if force:
+                if parents:
+                    _LOG.debug('Active linked parents found, deleting')
+                    for parent in parents:
+                        _LOG.debug(f'Force deleting parent {parent.parent_id}')
+                        self.parent_service.force_delete(parent=parent)
                 self.application_service.force_delete(
                     application=target_application)
             else:
+                if parents:
+                    active_parent_ids = [parent.parent_id for parent in
+                                         parents]
+                    message = (
+                        f'Can\'t delete application with active parents: '
+                        f'{", ".join(active_parent_ids)}')
+                    _LOG.error(message)
+                    return build_response(
+                        code=RESPONSE_BAD_REQUEST_CODE,
+                        content=message
+                    )
                 self.application_service.mark_deleted(
                     application=target_application)
         except ModularException as e:
